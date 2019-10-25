f2efbbd3220637cfa020771ede718b8a87842fb2
Manjusaka <me@manjusaka.me>
2019-10-26 02:01:10 +0800

    add slide

---
+++ source/code1.py
@@ -0,0 +1,15 @@
+class Simple1Exception(Exception):
+    pass
+
+
+class Simple2Exception(Exception):
+    pass
+
+
+def abc():
+    try:
+        raise Simple1Exception
+    except Simple1Exception as e:
+        raise Simple2Exception from e
+
+abc()
